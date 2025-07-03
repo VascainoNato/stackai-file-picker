e1bcf530c6d45860ff4d84ed55dcc5d800023764
rafaelsatyro <rafael.satyro@hotmail.com>
2025-07-03 19:09:23 -0300

    fix: sidebar-width-correction

--- src/app/components/LeftBar/index.tsx
+++ src/app/components/LeftBar/index.tsx
@@ -1,6 +1,6 @@
 export default function LeftBar() {
     return (
-      <div className="flex h-20 bg-red-500 h-full">
+      <div className="flex w-[10%] h-20 bg-gray-200 h-full">
           Side Menu
       </div>
     );
