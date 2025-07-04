cf7c5873cac45f21a664cfc342539635a1423082
rafaelsatyro <rafael.satyro@hotmail.com>
2025-07-04 17:04:18 -0300

    feat: inserted-different-layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,30 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div className="flex w-full h-screen flex-col bg-[#F9FAFB]">
+    <>
+    <div className=" flex w-full h-screenflex-col bg-[#F9FAFB] lg:hidden">
+      <div className="flex hidden md:flex-col md:flex-col">
+        <LeftBar />
+      </div>
+      <div className="flex w-full flex-col  ">
+        <Header />
+        <div className="flex w-full flex-grow">
+          <Content />
+        </div>
+        <Footer />
+      </div>
+    </div>
+    <div className="flex w-full h-screen flex-col bg-[#F9FAFB] hidden lg:flex">
       <div className="flex w-full h-screen">
-        <LeftBar/>
+        <LeftBar />
         <div className="flex w-full flex-col">
-          <Header/>
+          <Header />
           <div className="flex w-full flex-grow">
             <Content />
           </div>
-          <Footer/>
+          <Footer />
         </div>
       </div>
-    </div>
+    </div></>
   );
 }
