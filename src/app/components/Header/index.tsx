88355b65f7876a538b59c579d52426301285dd65
rafaelsatyro <rafael.satyro@hotmail.com>
2025-07-04 17:04:52 -0300

    feat: added-mobile-first-header

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,7 +1,44 @@
+import { Grid, Menu, MoreVertical, Plus, Search } from "lucide-react";
+import Image from "next/image";
+
 export default function Header() {
     return (
-      <div className="flex w-full h-20 bg-blue-500">
-          Header
+      <div className="flex w-full h-16 bg-[#c1c1c1] lg:bg-white lg:h-20">
+        <div className="flex w-full h-full px-6 items-center justify-between lg:px-8 xl:px-16">
+          <div className="flex gap-6 lg:justify-between lg:w-full lg:h-20 items-center lg:border-b-2">
+            <button className="flex items-center lg:hidden">
+              <Menu size={24} color="gray"/>
+            </button>
+            <h1 className="flex text-black font-normal text-xl lg:hidden font-roboto ">My Drive</h1>
+            <div className="hidden lg:flex items-center px-6 lg:px-0 gap-2">
+              <Image
+                src="/drive.webp"
+                alt="Google Drive"
+                width={30}
+                height={30}
+              />
+              <h1 className="text-md font-roboto font-semibold text-[color:#202124]">Google Drive</h1>
+              <div className="flex items-center p-1 rounded bg-gray-200 text-gray-500 ml-2 font-roboto text-sm">
+                Beta
+              </div>
+            </div>
+            <div className="flex items-center px-2 py-1 rounded bg-gray-200 text-gray-500 font-roboto gap-2 text-sm hidden lg:flex  items-center align-center justify-center">
+              <Plus size={16} color="gray"/>
+              Add Account
+            </div>
+          </div>
+          <div className="flex gap-6 lg:hidden">
+            <button className="flex items-center lg:hidden">
+              <Search size={22} color="gray"/>
+            </button>
+            <button className="flex items-center lg:hidden">
+             <Grid size={24} color="gray"/>
+            </button>
+            <button className="flex items-center lg:hidden">
+             <MoreVertical size={24} color="gray" />
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
