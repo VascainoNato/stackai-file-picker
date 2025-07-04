a8e06402a09e43ce457af73b47177cfa084367d7
rafaelsatyro <rafael.satyro@hotmail.com>
2025-07-04 20:34:30 -0300

    feat: added-overflow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,24 @@ import "./globals.css";
 export default function Home() {
   return (
     <>
-    <div className=" flex w-full h-screenflex-col bg-[#F9FAFB] lg:hidden">
+    <div className=" flex w-full min-h-screen flex-col bg-gray-200 lg:hidden">
       <div className="flex hidden md:flex-col md:flex-col">
         <LeftBar />
       </div>
       <div className="flex w-full flex-col  ">
         <Header />
-        <div className="flex w-full flex-grow">
+        <div className="flex w-full flex-grow overflow-y-auto max-h-screen">
           <Content />
         </div>
         <Footer />
       </div>
     </div>
-    <div className="flex w-full h-screen flex-col bg-[#F9FAFB] hidden lg:flex">
+    <div className="flex w-full h-screen flex-col bg-white hidden lg:flex">
       <div className="flex w-full h-screen">
         <LeftBar />
         <div className="flex w-full flex-col">
           <Header />
-          <div className="flex w-full flex-grow">
+          <div className="flex w-full flex-grow overflow-y-auto max-h-screen">
             <Content />
           </div>
           <Footer />
